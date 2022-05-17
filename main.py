86021e2d70894604902d23add4896187900dac77
Alec German <kimarimt@protonmail.com>
2022-05-17 08:04:14 -0400

    implemented secret auction

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+import os
+import time
+
+
+def clear():
+    time.sleep(1)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 def main():
-    print('Hello, World')
+    print('Welcome to the secret auction program.')
+    bids = {}
+    again = ''
+
+    while again != 'no':
+        bidder = input('Enter your name: ')
+        bid = int(input('Enter your bid: $'))
+        bids[bidder] = bid
+        again = input('\nAre there any other bidders(\'yes\' or \'no\'): ')
+        clear()
+
+    winner = ''
+    max_bid = 0
+
+    for name, amount in bids.items():
+        if amount > max_bid:
+            winner = name
+            max_bid = amount
+
+    print(f'The winner is {winner} with a bid of ${max_bid:.2f}')
 
 
 if __name__ == '__main__':
